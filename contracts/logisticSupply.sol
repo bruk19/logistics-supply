1b425ffcb7c64e8c16f3622ab3a7bbe319589913
Bruk Teshome <brukteshome21@gmail.com>
2024-06-12 19:44:11 +0300

    Add state variable

--- contracts/logisticSupply.sol
+++ contracts/logisticSupply.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.24;
 contract LogisticSupply {
   address public owner;
+  uint public medCount = 0;
+  uint public rawMatCount = 0;
+  uint public manuCount = 0;
+  uint public distCount = 0;
+  uint public retailCount = 0;
 
   constructor() {
     owner = msg.sender;
