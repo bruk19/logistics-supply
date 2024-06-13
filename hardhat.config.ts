c313df0ad076a4771ab61d23704b11d3f8339e60
Bruk Teshome <brukteshome21@gmail.com>
2024-06-13 11:24:23 +0300

    Private Key: import sepolia, etherscan and private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
